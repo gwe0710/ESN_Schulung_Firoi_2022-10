a6b9f6928e931adbf825e11b02bf27314e7421d1
sapux06 clouddnagmbhgmail.onmicrosoft.com <sapux06@clouddnagmbhgmail.onmicrosoft.com>
2022-10-15 06:27:17 +0000

    End of lessons

--- webapp/controller/Customer.controller.js
+++ webapp/controller/Customer.controller.js
@@ -1,19 +1,74 @@
 sap.ui.define([
     "sap/ui/core/mvc/Controller",
-    "sap/m/MessageBox"
+    "sap/m/MessageBox",
+    "sap/ui/model/json/JSONModel",
+    "sap/ui/core/Fragment",
+    "sap/ui/core/routing/History"
 ],
     /**
      * @param {typeof sap.ui.core.mvc.Controller} Controller
      */
-    function (Controller, MessageBox) {
+    function (Controller, MessageBox, JSONModel, Fragment, History) {
         "use strict";
 
         return Controller.extend("at.clouddna.student06.zhoui5.controller.Customer", {
-            onInit: function () {
 
+            _fragmentList: {},
+            bCreate: false,
+
+            onInit: function () {
+                let oEditModel = new JSONModel({
+                    editmode: false
+                });
+
+                this.getView().setModel(oEditModel, "editModel");
+
+                let oRouter = this.getOwnerComponent().getRouter();
+
+                oRouter.getRoute("Customer")
+                    .attachPatternMatched(this._onPatternMatched, this);
+
+                oRouter.getRoute("CreateCustomer")
+                    .attachPatternMatched(this._onCreatePatternMatched, this);
             },
 
-            genderFormatter: function(sKey){
+            _onPatternMatched: function (oEvent) {
+                this.bCreate = false;
+
+                let sPath = oEvent.getParameters().arguments.path;
+                this.sCustomerPath = "/" + sPath;
+                this.getView().bindElement(this.sCustomerPath);
+
+                this.getView().getModel("editModel").setProperty("/editmode", false);
+                this._showCustomerFragment("DisplayCustomer");
+            },
+
+            _onCreatePatternMatched: function (oEvent) {
+                this.bCreate = true;
+
+                let oNewCustomerContext = this.getView().getModel()
+                    .createEntry("/CustomerSet");
+                this.getView().bindElement(oNewCustomerContext.getPath());
+
+                this.getView().getModel("editModel").setProperty("/editmode", true);
+                this._showCustomerFragment("ChangeCustomer");
+            },
+
+
+            onNavBack: function () {
+                var oHistory = History.getInstance();
+                var sPreviousHash = oHistory.getPreviousHash();
+
+                //debugger;
+                if (sPreviousHash !== undefined) {
+                    window.history.go(-1);
+                } else {
+                    var oRouter = this.getOwnerComponent().getRouter();
+                    oRouter.navTo("Main");
+                }
+            },
+
+            genderFormatter: function (sKey) {
                 let oView = this.getView();
                 let oI18nModel = oView.getModel("i18n");
                 let oResourceBundle = oI18nModel.getResourceBundle();
@@ -21,23 +76,81 @@ sap.ui.define([
                 return sText;
             },
 
-            OnSavePressed: function() {
-                //alert("Alert")    ;
-                let oView = this.getView();
-
-                let oModel = oView.getModel();
+            onSavePressed: function () {
+                let oModel = this.getView().getModel();
+                let oResourceBundle = this.getView().getModel("i18n").getResourceBundle();
+                let sSuccessText = this.bCreate ?
+                    oResourceBundle.getText("dialog.create.success") :
+                    oResourceBundle.getText("dialog.edit.success");
+                /*
+                
                 let oData = oModel.getData();
+                MessageBox.success(JSON.stringify(oData));
+                this._toggleEdit(false);
+                */
+                oModel.submitChanges({
+                    success: (oData, response) => {
+                        MessageBox.success(sSuccessText, {
+                            onClose: () => {
+                                if (this.bCreate) {
+                                    this.onNavBack();
+                                } else {
+                                    this._toggleEdit(false);
+                                }
+                            }
+                        });
+                    },
+                    error: (oError) => {
+                        MessageBox.error(oError.message);
+                    }
+                });
+            },
+
+            onCancelPressed: function () {
+                let oModel = this.getView().getModel();
+                oModel.resetChanges().then(() => {
+                    if (this.bCreate) {
+                        this.onNavBack();
+                    } else {
+                        this._toggleEdit(false);
+                    }
+                });
+            },
 
 
-                //let oInputCustomerID = oView.byID("app_text_customerid")
+            onEditPressed: function () {
+                this._toggleEdit(true);
+            },
 
-                //let sCustomerID = oInputCustomerID.value
+            _toggleEdit: function (bEditMode) {
+                let oEditModel = this.getView().getModel("editModel");
 
-                console.log("Customerid: " & sCustomerID)
+                oEditModel.setProperty("/editmode", bEditMode);
 
-                MessageBox.success(JSON.stringify(oData))        
+                this._showCustomerFragment(bEditMode ? "ChangeCustomer" : "DisplayCustomer");
+            },
 
-            }    
+            onCancelPressed: function () {
+                this._toggleEdit(false);
+            },
 
+            _showCustomerFragment: function (sFragmentName) {
+                let oPage = this.getView().byId("page");
+
+                oPage.removeAllContent();
+
+                if (this._fragmentList[sFragmentName]) {
+                    oPage.insertContent(this._fragmentList[sFragmentName]);
+                } else {
+                    Fragment.load({
+                        id: this.getView().createId(sFragmentName),
+                        name: "at.clouddna.student06.zhoui5.view.fragment." + sFragmentName,
+                        controller: this
+                    }).then(function (oFragment) {
+                        this._fragmentList[sFragmentName] = oFragment;
+                        oPage.insertContent(this._fragmentList[sFragmentName]);
+                    }.bind(this));
+                }
+            }
         });
     });
--- webapp/controller/Main.controller.js
+++ webapp/controller/Main.controller.js
@@ -1,9 +1,12 @@
 sap.ui.define([
     "sap/ui/core/mvc/Controller",
-    "sap/m/MessageBox"
-    ],
+    "sap/m/MessageBox",
+    "sap/ui/model/json/JSONModel",
+    "sap/ui/core/Fragment",
+    "sap/ui/core/routing/History"
+],
 
-    function (Controller, MessageBox) {
+    function (Controller, MessageBox, JSONModel, Fragment, History) {
         "use strict";
 
         return Controller.extend("at.clouddna.student06.zhoui5.controller.Main", {
@@ -12,33 +15,68 @@ sap.ui.define([
 
             },
 
-            onDeleteButtonPressed: function(oEvent){
+            _showCustomerFragment: function (sFragmentName) {
+                let oPage = this.getView().byId("page");
+
+                oPage.removeAllContent();
+
+                if (this._fragmentList[sFragmentName]) {
+                    oPage.insertContent(this._fragmentList[sFragmentName]);
+                } else {
+                    Fragment.load({
+                        id: this.getView().createId(sFragmentName),
+                        name: "at.clouddna.student06.zhoui5.view.fragment." + sFragmentName,
+                        controller: this
+                    }).then(function (oFragment) {
+                        this._fragmentList[sFragmentName] = oFragment;
+                        oPage.insertContent(this._fragmentList[sFragmentName]);
+                    }.bind(this));
+                }
+            },
+
+            onCreatePressed: function () {
+                let oRouter = this.getOwnerComponent().getRouter();
+                oRouter.navTo("CreateCustomer", null, false);
+            },
+
+
+            onListItemPressed: function (oEvent) {
+                let sPath = oEvent.getSource().getBindingContext().getPath();
+                let oRouter = this.getOwnerComponent().getRouter();
+                debugger;
+                oRouter.navTo("Customer", {
+                    path: encodeURIComponent(sPath)
+                });
+            },
+
+            onDeleteButtonPressed: function (oEvent) {
                 let oModel = this.getView().getModel();
                 let oResourceBundle = this.getView().getModel("i18n").getResourceBundle();
-            
+
                 let oSource = oEvent.getSource();
                 let sPath = oSource.getBindingContext().getPath();
-                
+
                 //debugger;
 
-                _delete( sPath );
+                _delete(sPath);
 
             },
 
             _delete: function (sPath) {
                 let oModel = this.getView().getModel();
-                
+
                 MessageBox.warning(oResourceBundle.getText("sureToDeleteQuestion"), {
                     title: oResourceBundle.getText("sureToDeleteTitle"),
                     actions: [MessageBox.Action.YES, MessageBox.Action.NO],
                     emphasizedAction: MessageBox.Action.YES,
-                    onClose: function(oAction){
-                       
-                        if(MessageBox.Action.YES === oAction){
+                    onClose: function (oAction) {
+
+                        if (MessageBox.Action.YES === oAction) {
                             oModel.remove(sPath);
                         }
                     }
-                });            }
+                });
+            }
 
         });
     });
